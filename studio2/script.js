4a4051d498af014a5d1190b03e5267a47d3a79f5
Shehreen Husain <shusain@ucdavis.edu>
2019-02-06 09:16:39 -0800

    studio 2

---
+++ studio2/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('reading.js');
+
+/*click to show the corny joke and then click again to close*/
+
+/*change h1 text to "seriously" after you touch laptop*/
+
+/*change h1 text to "bitch" after you touch notebook*/
+
+/*change h1 text to "fine" after you touch keys*/
