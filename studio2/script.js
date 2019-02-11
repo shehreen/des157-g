8df585a9ffa1c4947e4f8d769847472af4b68e4c
Shehreen Husain <shusain@ucdavis.edu>
2019-02-10 23:39:34 -0800

    character face changes

--- studio2/script.js
+++ studio2/script.js
@@ -4,14 +4,33 @@ console.log('reading.js');
 
 /*click to show the corny jokes via overlay and then click again to close*/
 
-/*change h1 text to "seriously" after you touch laptop*/
-
-var headerText = 'don&#8242t touch my stuff';
-
 function headerSwap(){
-  document.querySelector('#header1').innerHTML = "seriously";
+  // console.log("Character clicked");
+  var headerTexts = document.getElementById('header1');
+  var character = document.getElementById('character');
+  var fire = document.getElementById('fire');
+  // console.log(character);
+
+  if (header1.innerHTML == "don't touch my stuff"){
+    header1.innerHTML = "seriously";
+  } else if ( header1.innerHTML == "seriously") {
+    header1.innerHTML = "bitch";
+    character.src = 'images/charactersad.svg'
+  } else if ( header1.innerHTML == "bitch") {
+    header1.innerHTML = "@#@$#%#%";
+    character.src = 'images/charactersad.svg';
+    fire.style.display="block";
+
+  } else if (header1.innerHTML == "@#@$#%#%") {
+    header1.innerHTML =  "don't touch my stuff";
+    character.src = 'images/character.svg'
+    fire.style.display="none";
+  }
+
 
 }
+
+//document.querySelector('#header1').innerHTML = "seriously";
 
 /*change h1 text to "bitch" after you touch notebook*/
 
