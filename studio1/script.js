ede1261bf3fecb1a51043cd95b57dcd1de1c1c44
shehreen <shusain@ucdavis.educd>
2020-06-08 14:53:41 -0700

    updated links + fixed madlibs

--- studio1/script.js
+++ studio1/script.js
@@ -23,13 +23,16 @@ document.getElementById("myform").addEventListener("submit", function(event){
     //run a function to put the words into the madlib... Function below...
     //Pass the array of words the user typed into the function
     makeMadLib(words);
+    document.querySelector(".form").style.display="none";
 
 });
 
 //This function takes an array input, then constructs the madlib, peppering
 //in the user's words where necessary.
 function makeMadLib(theWords){
-    var madlib = `Saying "All Lives Matter" is like saying ${theWords[0]} lives matter. It's unproductive and unncessary, because ${theWords[0]} lives are not under attack. On top of that, the police in ${theWords[1]} are impartial towards people of color due to deep rooted racial bias enabled by centuries of systemic oppression. ${theWords[2]} doesn't even begin to describe the kind of person that abuses the very people they took an oath to protect and serve. Surveys show the average recruit receives only eight hours of de-escalation training. It likely requires much more training time to become a  ${theWords[3]} than a police officer in ${theWords[1]} - just look it up!`;
+    var madlib = `Saying "All Lives Matter" is like saying ${theWords[0]} lives matter. It's unproductive and unncessary, because ${theWords[0]} lives are not under attack. On top of that, the police in ${theWords[1]} are impartial towards people of color due to deep rooted racial bias enabled by centuries of systemic oppression. ${theWords[2]} doesn't even begin to describe the kind of person that abuses the very people they took an oath to protect and serve. Surveys show the average recruit receives only eight hours of de-escalation training. It likely requires much more training time to become a  ${theWords[3]} than a police officer in ${theWords[1]} - just look it up!
+    <img id="leftimage" src ="images/juicy.svg">
+    `;
 
     //Get the madlib container
     var mlContainer = document.getElementById('madlib');
